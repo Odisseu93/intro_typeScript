d59afd0ca7c1c260bfed22f2e5bd237771597f1a
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-07-07 11:50:02 -0300

    alterando a funcao adiciona() e criando a funcao criaNegociacao()

--- dist/js/controllers/negociacao-crontroller.js
+++ dist/js/controllers/negociacao-crontroller.js
@@ -1,12 +1,19 @@
+import { Negociacao } from "../models/negociacao.js"; //tem que colocal o ".js" mesmo com o autoImport
 export class NegociacaoController {
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
-    adicona() {
-        console.log(this.inputData);
-        console.log(this.inputQuantidade);
-        console.log(this.inputValor);
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+    }
+    criaNegociacao() {
+        const exp = /-/g; // Regular expressions https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+        const date = new Date(this.inputData.value.replace(exp, ',')); // quando a exp. regular encontrar o valor, irá substituir por ','
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
     }
 }
