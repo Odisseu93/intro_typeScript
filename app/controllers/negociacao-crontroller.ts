fdf41f3e77247f95c1c03f6a972cf88d60277f65
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-07-07 10:36:11 -0300

    organizando a classe

--- app/controllers/negociacao-crontroller.ts
+++ app/controllers/negociacao-crontroller.ts
@@ -1,7 +1,9 @@
+import { Negociacao } from "../models/negociacao.js"; //tem que colocal o ".js" mesmo com o autoImport
+
 export class NegociacaoController {
-  private inputData;
-  private inputQuantidade;
-  private inputValor;
+  private inputData: HTMLInputElement;
+  private inputQuantidade: HTMLInputElement;
+  private inputValor: HTMLInputElement;
 
   constructor() {
     this.inputData = document.querySelector('#data');
@@ -10,9 +12,18 @@ export class NegociacaoController {
 
   }
 
-  adicona() {
-    console.log(this.inputData);
-    console.log(this.inputQuantidade)
-    console.log(this.inputValor)
+  adiciona(): void {
+    const negociacao = this.criaNegociacao();
+    console.log(negociacao)
+  }
+  
+  criaNegociacao(): Negociacao {
+    
+    const exp = /-/g; // Regular expressions https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+    const date = new Date(this.inputData.value.replace(exp, ',')); // quando a exp. regular encontrar o valor, irá substituir por ','
+    const quantidade = parseInt(this.inputQuantidade.value);
+    const valor = parseFloat(this.inputValor.value);
+    return new Negociacao(date, quantidade, valor);
+    
   }
 }
