e6b1e1fe1ae2a8a11a0241df362523c69fd44c5c
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-07-07 11:36:56 -0300

    adicionando um evento e uma acao para o botao

---
+++ app/app.ts
@@ -0,0 +1,11 @@
+import { NegociacaoController } from "./controllers/negociacao-crontroller.js";
+
+
+
+const controller = new NegociacaoController();
+const form = document.querySelector('.form'); // Event Bubbling https://riptutorial.com/dom/example/1344/event-bubbling-and-capturing
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  controller.adiciona();
+});
+
