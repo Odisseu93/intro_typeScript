0eab77f3753666b3758880f764798c56021560b2
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-07-11 20:23:55 -0300

    adicionando e imprementando os metodos adiciona() e lista() da class Negociacoes

--- app/controllers/negociacao-crontroller.ts
+++ app/controllers/negociacao-crontroller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js"; //tem que colocal o ".js" mesmo com o autoImport
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
+  private negociacoes = new Negociacoes();
 
   constructor() {
     this.inputData = document.querySelector('#data');
@@ -14,7 +16,9 @@ export class NegociacaoController {
 
   adiciona(): void {
     const negociacao = this.criaNegociacao();
-    console.log(negociacao);
+    this.negociacoes.adiciona(negociacao);
+    this.negociacoes.lista().pop();
+    console.log(this.negociacoes.lista());
     this.limparFormulario();
   }
   
