bde370f4551413c8aa571ea2c9e395929d7137b4
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-07-13 19:24:18 -0300

    protegendo o valor da data para nao ser alterado

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,7 +1,7 @@
 export class Negociacao {
 
   constructor(
-    public readonly data: Date,
+    private _data: Date,
     public readonly quantidade: number,
     public readonly valor: number
   ) {}
@@ -11,5 +11,9 @@ export class Negociacao {
     return this.quantidade * this.valor;
   }
 
+  get data(): Date {
+      const data = new Date(this._data.getTime());
+      return data; // as alterações agora, serão somente na cópia da data e não na data em si.
+  }
 
 }
