62fb9459c426541f362149e24a28155e03e7e0a3
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-07-13 18:31:07 -0300

    setando os atributos da classe como public readonly

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,27 +1,10 @@
 export class Negociacao {
-    //  private _data: Date;
-    //  private _quantidade: number;
-    //  private _valor: number;
-    // constructor (data: Date, quantidade: number, valor: number){
-    //   this._data = data;
-    //   this._quantidade = quantidade;
-    //   this._valor = valor;
-    // }
-    constructor(_data, _quantidade, _valor) {
-        this._data = _data;
-        this._quantidade = _quantidade;
-        this._valor = _valor;
-    }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
+    constructor(data, quantidade, valor) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get volume() {
-        return this._quantidade * this.valor;
+        return this.quantidade * this.valor;
     }
 }
