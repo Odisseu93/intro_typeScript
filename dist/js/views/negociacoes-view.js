d651c2a87223e60c1acda2f5338fc21e7b686a0a
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-07-14 20:31:09 -0300

    adicionando a primeira view

---
+++ dist/js/views/negociacoes-view.js
@@ -0,0 +1,27 @@
+export class NegociacoesView {
+    constructor(seletor) {
+        this.elemento = document.querySelector(seletor);
+    }
+    template() {
+        return `
+      <table class="table table-hover table-bordered">
+        <thead>
+            <th>
+                DATA
+            </th>
+            <th>
+                QUANTIDADE
+            </th>
+            <th>
+                VALOR
+            </th>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    `;
+    }
+    update() {
+        this.elemento.innerHTML = this.template();
+    }
+}
